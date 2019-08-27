aba9ab7199ef625ca950dc39b8dff93efe22f421
shehab hosny <shehab_xc@hotmail.com>
2019-08-27 12:38:50 +0200

    first configration

--- resources/assets/js/components/Layout/Header.vue
+++ resources/assets/js/components/Layout/Header.vue
@@ -1,4 +1,5 @@
 <template>
+ <!-- hellow first change to push -->
   <!-- START NAV -->
   <nav class="navbar is-white is-fixed-top">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
