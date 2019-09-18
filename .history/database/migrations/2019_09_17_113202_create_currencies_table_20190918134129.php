6a359dfb4eb7f93b25159b2725eb91ee8a81111d
your username <eslamelshenawy@gmail.com>
2019-09-18 14:51:29 +0200

    after add food ordering

--- .history/database/migrations/2019_09_17_113202_create_currencies_table_20190918134129.php
+++ .history/database/migrations/2019_09_17_113202_create_currencies_table_20190918134129.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFailedJobsTable extends Migration
+class CreateCurrenciesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ class CreateFailedJobsTable extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('currencies', function (Blueprint $table) {
             $table->increments('id');
-            
+            $table->string('name')->nullable($value=true);
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ class CreateFailedJobsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('currencies');
     }
 }
