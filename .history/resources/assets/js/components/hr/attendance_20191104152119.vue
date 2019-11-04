62388887430b63c17788f12f138d1614427e7bad
shehab hosny <shehab_xc@hotmail.com>
2019-11-04 18:14:01 +0200

    download ex cheet success working on date list

---
+++ .history/resources/assets/js/components/hr/attendance_20191104152119.vue
@@ -0,0 +1,150 @@
+<template>
+<section class="apps container">           
+      
+    <sidebarmenu></sidebarmenu>
+
+    <div>
+        <b>Upload Attendance File</b>
+            <div class="level">
+                <div class="level-item">
+                    <router-link to="/admin/vue/uploadLeads" class="button is-success is-meduim mr-10 import-excel">
+                        Import From Excel
+                    </router-link>
+                    <router-link :to="'/admin/vue/newLead'"  @click="showAddLead = true" class="button is-success is-meduim mr-10">
+                        <span> Add new... </span>
+                    </router-link>
+                </div>
+            </div>
+    </div>
+    <hr>
+
+    <b-field>
+        <b-upload v-model="dropFiles"
+            multiple
+            @input="handleFile"
+            drag-drop>
+            <section class="section">
+                <div class="content has-text-centered">
+                    <p>
+                        <b-icon
+                            icon="upload"
+                            size="is-large">
+                        </b-icon>
+                    </p>
+                    <p>Upload File To Server</p>
+                </div>
+            </section>
+        </b-upload>
+    </b-field>
+
+    <b-field class="file">
+        <div class="tags">
+            <span v-for="(file, index) in dropFiles"
+                :key="index"
+                class="tag is-primary" >
+                {{file.name}}
+                <button class="delete is-small"
+                    type="button"
+                    @click="deleteDropFile(index)">
+                </button>
+            </span>
+        </div>
+    </b-field>
+
+    <b-button type="is-primary" @click="SubmitSheet">
+            <b-icon icon="Send"></b-icon>
+            <span>Upload</span>
+    </b-button>
+    <div id="app" class="column is-5" style="margin-top:10%;">
+         <progress-bar size="large" bar-color="#dc720f" val="0" text="0%"></progress-bar>
+    </div>
+    
+      </section>
+</template>
+
+<style>
+.navMenu a[data-v-7f183654]{
+    background-color: #fff !important;
+    color: #888;
+}
+.navMenu[data-v-7f183654]{
+    background-color: #fff;
+    margin-top: 3rem;
+}
+.table{
+    width:100% ;
+    margin-bottom:2%;
+}
+  .btnAdd{
+    background-color: #00a65a;
+    color: #fff;
+    border: none;
+    width: 50px;
+    height: 30px;
+    float: right;
+    margin-bottom: 5PX;
+  }
+  .search{
+      float: right;
+  }
+  .add{
+   float: right;
+  }
+  .inputSearch{
+    height: 30px;
+    margin-bottom: 10PX;
+  }
+  .apps{
+      background-color: #fff;
+      padding: 1% !important;
+  }
+ 
+  </style>
+
+<script src="https://unpkg.com/@jeremyhamm/vue-slider"></script>
+<script>
+
+import Vue from 'vue'
+import sidebarmenu from './sidebarmenu'
+import ProgressBar from 'vue-simple-progress'
+
+import {StoreAttendanceByEx} from './../../calls'
+export default {
+    data() {
+            return {
+                file:null,
+                dropFiles: [],
+            }
+        },
+        components: {
+            'sidebarmenu': sidebarmenu,
+             file: null
+        },
+         methods:{
+             SubmitSheet(){
+                 const bodyFormData = new FormData
+                 bodyFormData.append('Employee_Sheet',this.dropFiles[0])
+                StoreAttendanceByEx(bodyFormData).then(response=>{
+                    console.log(response)
+                }).catch(error=>{
+                    console.log(error)
+                })
+             },
+            deleteDropFile(index) {
+                this.dropFiles.splice(index, 1)
+            },
+            handleFile(event) {
+                const file = event
+                console.log(file)
+                if (!/\.(xls|xlsx|csv)$/i.test(file[0].name)) {
+                    alert('Your choice is not an excel sheet')
+                    return prevent()
+                }
+                var reader = new FileReader
+                reader.addEventListener('load', () => {
+                    this.uploadedFile = reader.result
+                })
+            },
+        },
+}
+</script>
---
+++ .history/resources/views/admin/employee/attendance_cheet_20191104164952.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Export</title>
+    <style>
+        #head{
+            background-color: #000;color: #fff;
+            font-size: 17px;
+            font-weight: bold;
+            padding: 15px;
+        }
+    </style>
+</head>
+<body>
+<table>
+    <tr id="head">
+    <th>Date</th>
+        <th>Name</th>
+        <th>Time in</th>
+        <th>Time out</th>
+        <th>comment</th>
+        <th>Employee id</th>
+    </tr>
+</table>
+
+
+</body>
+</html>
